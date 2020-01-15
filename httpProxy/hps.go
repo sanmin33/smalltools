eb579f7235989a6ad305a7d2e67cf861e4c0d9d6
sanmin33 <cnljh@163.com>
2020-01-15 16:09:52 +0800

    修改为客户端和服务端，客户端和服务端采用RC4加密传输功能，这是服务端

--- httpProxy/hps.go
+++ httpProxy/hps.go
@@ -1,122 +1,156 @@
+/*加密传输的proxy，采用RC4加密，
+*/
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "io"
-        "log"
-        "net"
-        "net/http"
-        "strings"
+	"bufio"
+	"crypto/rc4"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"runtime"
+	"strings"
 )
 
+type Rc4 struct {
+	C *rc4.Cipher
+}
+
+var pwd string = "helloworld"
+var fport = flag.String("p", "8080", "侦听的端口号")
+var port string
+
+func init() {
+
+	flag.Parse()
+	port = ":" + *fport
+}
+
 func main() {
-        log.SetFlags(log.LstdFlags | log.Lshortfile)
-        tcpaddr, err := net.ResolveTCPAddr("tcp4", ":8080")
-        if err != nil {
-                fmt.Println("侦听地址错",err)
-                return
-        }
-        tcplisten, err := net.ListenTCP("tcp", tcpaddr)
-        if err != nil {
-                fmt.Println("开始tcp侦听出错",err)
-        }
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	tcpaddr, err := net.ResolveTCPAddr("tcp4", port)
+	if err != nil {
+		fmt.Println("侦听地址错", err)
+		return
+	}
+	tcplisten, err := net.ListenTCP("tcp", tcpaddr)
+	if err != nil {
+		fmt.Println("开始tcp侦听出错", err)
+	}
 
-        for {
-                client, err := tcplisten.AcceptTCP()
-                if err != nil {
-                        log.Panic(err)
-                }
+	for {
+		client, err := tcplisten.AcceptTCP()
+		if err != nil {
+			log.Println("当前协程数量：", runtime.NumGoroutine)
+			log.Panic(err)
+		}
 
-                go handleAHttp(client)
-        }
+		go handleAServerConn(client)
+	}
+}
+func handleAServerConn(client *net.TCPConn) {
+	c1, _ := rc4.NewCipher([]byte(pwd))
+	c2, _ := rc4.NewCipher([]byte(pwd))
+
+	pcTos := &Rc4{c1} //从client端收到的是密文，这里的作用是解密
+	psToc := &Rc4{c2} //web服务器传过来的是明文，这里的作用是加密
+
+	if client == nil {
+		return
+	}
+	defer client.Close()
+	//对客户端传来的密文进行解密，得到http报头, 得到web服务器的ip、端口等信息
+	byteHeader := deCodereadSplitString(client, pcTos, []byte("\r\n\r\n"))
+	fmt.Println("原始报头信息：", string(byteHeader))
+
+	//取报头字节流后解析为结构化报头，方便获取想要的信息
+	bfr := bufio.NewReader(strings.NewReader(string(byteHeader)))
+	req, err := http.ReadRequest(bfr)
+	if err != nil {
+		log.Println("转换request失败", err)
+		return
+	}
+	var method, host, address string
+	method = req.Method
+	host = req.Host
+	//hostPortURL, err := url.Parse(host)
+	fmt.Println("取request信息m:", method, "host:", host) //, "hostPortURL:", hostPortURL)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//取服务器域名（或IP）和端口号以便tcp拨号服务器
+	hostPort := strings.Split(host, ":")
+	if len(hostPort) < 2 {
+		address = hostPort[0] + ":80"
+	} else {
+		address = host
+	}
+
+	fmt.Println("获得服务器地址address:", address)
+	//获得了请求的host和port，就开始拨号吧
+	tcpaddr, err := net.ResolveTCPAddr("tcp4", address)
+	if err != nil {
+		log.Println("tcp地址错误", address, err)
+		return
+	}
+	server, err := net.DialTCP("tcp", nil, tcpaddr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//服务端得到的报头，客户端已经处理过了，服务端只需要无脑转发信息给web服务器就可以了
+	server.Write(byteHeader[0:len(byteHeader)]) //最始的报头信息已经解密了，这里直接转发给web
+
+	//接下来得到的都是还没有解密的信息，进行解密转发
+	go pcTos.encryptCopy(server, client) //服务端收到的是密文，编码后就成了明文并传给web
+	psToc.encryptCopy(client, server)    //web发过来的是明文，编码后就成了密文，并传给客户端
 }
 
-func handleAHttp(client *net.TCPConn) {
-        if client == nil {
-                return
-        }
-        defer client.Close()
-        //取客户端字节流报头存着，以便转发给服务器
-        byteHeader := readSplitString(client, []byte("\r\n\r\n"))
-        fmt.Println("原始报头信息：", string(byteHeader))
+func deCodereadSplitString(r *net.TCPConn, coder *Rc4, delim []byte) []byte {
+	var rs []byte
+	lenth := len(delim)
+	//fmt.Println("demim lenth:", lenth, "delim:", delim)
+	curByte := make([]byte, 1)
 
-        //取报头字节流后解析为结构化报头，方便获取想要的信息
-        bfr := bufio.NewReader(strings.NewReader(string(byteHeader)))
-        req, err := http.ReadRequest(bfr)
-        if err != nil {
-                log.Println("转换request失败", err)
-                return
-        }
-        var method, host, address string
-        method = req.Method
-        host = req.Host
-        //hostPortURL, err := url.Parse(host)
-        fmt.Println("取request信息m:", method, "host:", host)//, "hostPortURL:", hostPortURL)
-        if err != nil {
-                log.Println(err)
-                return
-        }
-        //取服务器域名（或IP）和端口号以便tcp拨号服务器
-        hostPort := strings.Split(host, ":")
-        if len(hostPort) < 2 {
-                address = hostPort[0] + ":80"
-        } else {
-                address = host
-        }
+	//先读取分隔符长度-1个字节，以避免在下面循环中每次都要判断是否读够分隔符长度的字节。
+	for k := 0; k < lenth-1; k++ {
+		r.Read(curByte)
+		coder.C.XORKeyStream(curByte, curByte) //把密文转成明文
+		rs = append(rs, curByte[0])
+	}
 
-        fmt.Println("获得服务器地址address:", address)
-        //获得了请求的host和port，就开始拨号吧
-        tcpaddr, err := net.ResolveTCPAddr("tcp4", address)
-        if err != nil {
-                log.Println("tcp地址错误", address, err)
-                return
-        }
-        server, err := net.DialTCP("tcp", nil, tcpaddr)
-        if err != nil {
-                log.Println(err)
-                return
-        }
-        if method == "CONNECT" {
-                fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n\r\n")
-                fmt.Println("向客户端返回https完成连接信息")
-        } else {
-                server.Write(byteHeader[0:len(byteHeader)]) //如果不是https请求，则需要直接转发客户端请求给服务器
+	//继续读后面的字节并开始进行查找是否已经接收到报头正文分隔符
+	for n, err := r.Read(curByte); err == nil && n > 0; n, err = r.Read(curByte) {
+		coder.C.XORKeyStream(curByte, curByte)
+		rs = append(rs, curByte[0])
 
-        }
-        //进行转发
-        go io.Copy(server, client)
-        io.Copy(client, server)
+		var m int
+		//从后向前逐个字节比较已读字节的最后几位是否和分隔符相同
+		for m = 0; m < lenth; m++ {
+			tt := len(rs)
+			if rs[tt-1-m] != delim[lenth-1-m] {
+				break
+			}
+		}
+		if m == lenth {
+			return rs
+		}
+	}
+	return rs
 }
 
-//从字节流读到某一字符串为止,比如用于只从io口读http报头，报头和正文的分隔符为\r\n\r\n
-func readSplitString(r *net.TCPConn, delim []byte) []byte {
-        var rs []byte
-        lenth := len(delim)
-        //fmt.Println("demim lenth:", lenth, "delim:", delim)
-        curByte := make([]byte, 1)
+func (c *Rc4) encryptCopy(dst io.Writer, src io.Reader) {
+	buf := make([]byte, 4096)
+	var err error
+	n := 0
+	for n, err = src.Read(buf); err == nil && n > 0; n, err = src.Read(buf) {
+		c.C.XORKeyStream(buf[:n], buf[:n])
 
-        //先读取分隔符长度-1个字节，以避免在下面循环中每次都要判断是否读够分隔符长度的字节。
-        for k := 0; k < lenth-1; k++ {
-                r.Read(curByte)
-                rs = append(rs, curByte[0])
-        }
+		dst.Write(buf[:n])
+	}
 
-        //继续读后面的字节并开始进行查找是否已经接收到报头正文分隔符
-        for n, err := r.Read(curByte); err == nil && n > 0; n, err = r.Read(curByte) {
-                rs = append(rs, curByte[0])
-
-                var m int
-                //从后向前逐个字节比较已读字节的最后几位是否和分隔符相同
-                for m = 0; m < lenth; m++ {
-                        tt := len(rs)
-                        if rs[tt-1-m] != delim[lenth-1-m] {
-                                break
-                        }
-                }
-                if m == lenth {
-                        return rs
-                }
-        }
-        return rs
 }
