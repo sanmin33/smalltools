c971e95e73683cfc1dbe81704c976824939af879
sanmin33 <cnljh@163.com>
2020-01-16 17:21:31 +0800

    加入读配置文件功能

--- httpProxy/hps.go
+++ httpProxy/hps.go
@@ -7,10 +7,12 @@ import (
 	"crypto/rc4"
 	"flag"
 	"fmt"
+	"github.com/buger/jsonparser"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"runtime"
 	"strings"
 )
@@ -20,38 +22,64 @@ type Rc4 struct {
 }
 
 var pwd string = "helloworld"
-var fport = flag.String("p", "8080", "侦听的端口号")
+var ffilename = flag.String("f", "config.json", "配置文件名")
 var port string
+var fileName string
+var httpLen int = 4096
 
 func init() {
 
 	flag.Parse()
-	port = ":" + *fport
+	fileName = *ffilename
 }
 
 func main() {
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("打开配置文件失败")
+		return
+	}
+	var jsondata []byte
+	var buf = make([]byte, 1)
+	for n, err := f.Read(buf); err == nil && n > 0; n, err = f.Read(buf) {
+		jsondata = append(jsondata, buf[0])
+	}
+	port, err = jsonparser.GetString(jsondata, "localPort")
+	if err != nil {
+		fmt.Println("配置文件中无服务端口号", port, err)
+		return
+	}
+	port = ":" + port
+	pwd, err = jsonparser.GetString(jsondata, "password")
+	if err != nil {
+		fmt.Println("配置文件中无密码", err)
+		return
+	}
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	tcpaddr, err := net.ResolveTCPAddr("tcp4", port)
 	if err != nil {
-		fmt.Println("侦听地址错", err)
+		fmt.Println("侦听地址错", tcpaddr, err)
 		return
 	}
 	tcplisten, err := net.ListenTCP("tcp", tcpaddr)
 	if err != nil {
 		fmt.Println("开始tcp侦听出错", err)
 	}
-
+	fmt.Println("hps服务运行于", port)
 	for {
 		client, err := tcplisten.AcceptTCP()
 		if err != nil {
-			log.Println("当前协程数量：", runtime.NumGoroutine)
+			log.Println("当前协程数量：", runtime.NumGoroutine())
 			log.Panic(err)
 		}
 
+		log.Println("当前协程数量：", runtime.NumGoroutine())
 		go handleAServerConn(client)
 	}
 }
 func handleAServerConn(client *net.TCPConn) {
+
+	defer client.Close()
 	c1, _ := rc4.NewCipher([]byte(pwd))
 	c2, _ := rc4.NewCipher([]byte(pwd))
 
@@ -61,7 +89,6 @@ func handleAServerConn(client *net.TCPConn) {
 	if client == nil {
 		return
 	}
-	defer client.Close()
 	//对客户端传来的密文进行解密，得到http报头, 得到web服务器的ip、端口等信息
 	byteHeader := deCodereadSplitString(client, pcTos, []byte("\r\n\r\n"))
 	fmt.Println("原始报头信息：", string(byteHeader))
@@ -117,7 +144,6 @@ func handleAServerConn(client *net.TCPConn) {
 func deCodereadSplitString(r *net.TCPConn, coder *Rc4, delim []byte) []byte {
 	var rs []byte
 	lenth := len(delim)
-	//fmt.Println("demim lenth:", lenth, "delim:", delim)
 	curByte := make([]byte, 1)
 
 	//先读取分隔符长度-1个字节，以避免在下面循环中每次都要判断是否读够分隔符长度的字节。
@@ -125,13 +151,18 @@ func deCodereadSplitString(r *net.TCPConn, coder *Rc4, delim []byte) []byte {
 		r.Read(curByte)
 		coder.C.XORKeyStream(curByte, curByte) //把密文转成明文
 		rs = append(rs, curByte[0])
+		if len(rs) > httpLen {
+			return rs
+		}
 	}
 
 	//继续读后面的字节并开始进行查找是否已经接收到报头正文分隔符
 	for n, err := r.Read(curByte); err == nil && n > 0; n, err = r.Read(curByte) {
 		coder.C.XORKeyStream(curByte, curByte)
 		rs = append(rs, curByte[0])
-
+		if len(rs) > httpLen {
+			return rs
+		}
 		var m int
 		//从后向前逐个字节比较已读字节的最后几位是否和分隔符相同
 		for m = 0; m < lenth; m++ {
