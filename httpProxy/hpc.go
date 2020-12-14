7e511b30f6fe15635b6bd65b51109f16ed9bd20c
sanmin33 <cnljh@163.com>
2020-12-14 10:18:12 +0800

    Update hpc.go
    加入无流量超时主动闭关连接功能，防止文件打开太多错误。

--- httpProxy/hpc.go
+++ httpProxy/hpc.go
@@ -6,8 +6,11 @@ import (
 	"crypto/rc4"
 	"flag"
 	"fmt"
+	"time"
+
 	"github.com/buger/jsonparser"
-	"io"
+
+	//	"io"
 	"log"
 	"net"
 	"os"
@@ -85,6 +88,9 @@ func main() {
 		client, err := tcplisten.AcceptTCP()
 		if err != nil {
 			log.Println("当前协程数量：", runtime.NumGoroutine())
+			if client != nil {
+				client.Close()
+			}
 			log.Panic(err)
 		}
 
@@ -95,7 +101,7 @@ func main() {
 
 func handleAClientConn(client *net.TCPConn) {
 
-	defer client.Close()
+	//defer client.Close()
 	c1, _ := rc4.NewCipher([]byte(pwd))
 	c2, _ := rc4.NewCipher([]byte(pwd))
 	pcTos := &Rc4{c1}
@@ -118,15 +124,22 @@ func handleAClientConn(client *net.TCPConn) {
 		log.Println("拨号服务器失败", err)
 		return
 	}
-	//进行转发
+	//defer server.Close()
+	//进行转发,这两句顺序不能倒，否则tcp连接不会自动关掉，会越来越多，只有等系统的tcp,timout到来
+	//才能关闭掉。
+	go psToc.encryptCopy(client, server) //代理服务端发过来的是密文，编码后就成了明文，并传给浏览器
 	go pcTos.encryptCopy(server, client) //客户端收到的是明文，编码后就成了密文并传给代理的服务端
-	psToc.encryptCopy(client, server)    //代理服务端发过来的是密文，编码后就成了明文，并传给浏览器
 }
-func (c *Rc4) encryptCopy(dst io.Writer, src io.Reader) {
+func (c *Rc4) encryptCopy(dst *net.TCPConn, src *net.TCPConn) {
+	defer dst.Close()
+	defer src.Close()
 	buf := make([]byte, 4096)
 	var err error
 	n := 0
 	for n, err = src.Read(buf); err == nil && n > 0; n, err = src.Read(buf) {
+		//5秒无数据传输就断掉连接
+		dst.SetDeadline(time.Now().Add(time.Second * 5))
+		src.SetDeadline(time.Now().Add(time.Second * 5))
 		c.C.XORKeyStream(buf[:n], buf[:n])
 
 		dst.Write(buf[:n])
